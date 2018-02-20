00d68723090981d17e42c5aa0dbfba024478ef0b
cpasjuste <cpasjuste@gmail.com>
2018-02-20 10:16:41 +0100

    fix cpp

--- include/nxnetprint.h
+++ include/nxnetprint.h
@@ -5,10 +5,18 @@
 #ifndef NXNETPRINT_H
 #define NXNETPRINT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int nx_net_init(const char *ip, int port);
 
 void nx_net_print(const char *str, ...);
 
 void nx_net_exit();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //NXNETPRINT_H
